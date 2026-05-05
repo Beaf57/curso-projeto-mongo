aea23e0deb8360ed55e595128e42b8082cc4115b
Beatriz Farias Péres <devbeatrizfarias.bfp@gmail.com>
2026-05-05 15:58:16 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/devbeatriz/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.devbeatriz.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devbeatriz.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
